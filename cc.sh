2b68510a1253c6e3816ebf73eebdde2447d26b11
HyunGyu-Lee <gusrb0808@naver.com>
2019-09-28 02:19:51 +0900

    cc 추가

---
+++ cc.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+commitHash=f8ae65a3e37fca1bfb5ea7f6b9d681f73b04755f
+
+git filter-branch -f --env-filter \
+    'if [ $GIT_COMMIT = {$commitHash} ]
+     then
+         export GIT_AUTHOR_DATE="Sep 27 10:00 2019 +0900"
+         export GIT_COMMITTER_DATE="Sep 27 10:00 2019 +0900"
+     fi'
